a620965147e7f4a35f7c5a029cc652b81926686f
Tommy in Tongji <36354458+TommyZihao@users.noreply.github.com>
2018-12-04 20:38:31 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-sudo apt-get install lolcat -y
+cd
+sudo mv /etc/apt/sources.list /etc/apt/sources2.list 
+sudo mv /linux-funny-command/2.sh /etc/apt/sources.list
+sudo apt install lolcat -y
 echo -e '马上开始安装40个有趣的Linux命令行彩蛋和游戏\n这个脚本文件是同济大学子豪兄在2018年12月4日写的'| lolcat
 sleep 2
 echo '欢迎关注知乎专栏：人工智能小技巧'| lolcat
